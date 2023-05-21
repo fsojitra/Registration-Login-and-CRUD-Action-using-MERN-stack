eb04133fe87cf866c0e3a3ae1e25f6632cb5d59e
Foram Sojitra <foram.sojitra295@gmail.com>
2023-05-21 20:38:55 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route } from "react-router";
-import { BrowserRouter, Routes } from "react-router-dom";
+//import { Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./Login";
 import Register from "./Register";
